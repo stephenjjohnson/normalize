647cfd709e50ab299eb1402d4d2778eb13cbf0f9
Stephen Johnson <stephenjjohnson@gmail.com>
2015-03-17 09:18:22 -0500

    COnverted remaining variable names from var_name to varName format to distinguish from function names.

--- normalize.py
+++ normalize.py
@@ -9,16 +9,16 @@ Hitting ENTER at any of the prompts after the data have been loaded will set a s
 
 def main():
     # Get some data to normalize
-    # my_data = (0,1,2,3,4,5,6,7,8,9,10,11)
-    my_data = read_my_data(get_file())
-    print 'My data:', my_data, '\n'
+    # myData = (0,1,2,3,4,5,6,7,8,9,10,11)
+    myData = read_my_data(get_file())
+    print 'My data:', myData, '\n'
 
     # Get the smallest and largest data to use in the normalization
     # Smallest
-    my_default = str(min(my_data))
+    my_default = str(min(myData))
     minData = get_parameter('What is the smallest value to consider? [Enter to use smallest datum]:', my_default, 'float')
     # Largest
-    my_default = str(max(my_data))
+    my_default = str(max(myData))
     maxData = get_parameter('What is the largest value to consider? [Enter to use largest datum]:', my_default, 'float')
     print 'Data range to normalize:', minData, '-', maxData, '\n'
 
@@ -29,26 +29,26 @@ def main():
     print 'Normalized range: ', minNorm, '-', str(maxNorm), '\n'
     
     # Should we print the data out?
-    verbose_flag = get_parameter('Do you want to see the data while saving? (y/N) ', False, 'bool')
+    verboseFlag = get_parameter('Do you want to see the data while saving? (y/N) ', False, 'bool')
 
     # Normalize the data using the supplied parameters
-    my_norm_data = normalize(my_data, minData, maxData, minNorm, maxNorm)
+    my_norm_data = normalize(myData, minData, maxData, minNorm, maxNorm)
 #    print 'Normalized data:', my_norm_data
 
     # Save in a file
-    save_my_data('normalized', my_norm_data, verbose_flag)  
+    save_my_data('normalized', my_norm_data, verboseFlag)  
 
 
 def get_file() :
     '''
     Get a file name to examine
     '''
-    my_file = raw_input('Enter a file name: ')
+    myFile = raw_input('Enter a file name: ')
     try:
-        fhandle = open(my_file, 'r')
+        fhandle = open(myFile, 'r')
         return fhandle
     except:
-        print 'Invalid file name:', my_file
+        print 'Invalid file name:', myFile
         quit()
 
 
@@ -57,32 +57,32 @@ def read_my_data(fhandle):
     Read the data into a list
     '''
     print 'Loading data...'
-    my_data = list()
+    myData = list()
     for line in fhandle:
         data_items = line.split(',')
         for datum in data_items:
             datum = float(datum)
-            my_data.append(datum)
+            myData.append(datum)
     print 'Done.'
-    return my_data
+    return myData
 
 
-def get_parameter(prompt, val_default, val_type='str'):
+def get_parameter(prompt, valDefault, valType='str'):
     '''
-    Needs prompt, val_default and val_type as strings.
-    val_type can be 'str', 'int', 'bool' or 'float'
-    Returns val converted to val_type
+    Needs prompt, valDefault and valType as strings.
+    valType can be 'str', 'int', 'bool' or 'float'
+    Returns val converted to valType
     '''
     while True:
         val = raw_input(prompt)
         if val == '':
-            val = val_default
+            val = valDefault
         try:
-            if val_type == 'float':
+            if valType == 'float':
                 val = float(val)
-            elif val_type == 'int' :
+            elif valType == 'int' :
                 val = int(val)
-            elif val_type == 'bool' :
+            elif valType == 'bool' :
                 if val in ['Y','Yes','y','yes','true','True']:
                     val = True
                 else:
@@ -93,29 +93,29 @@ def get_parameter(prompt, val_default, val_type='str'):
             continue
     
     
-def normalize(my_data, minData, maxData, min_norm=0, max_norm=1):
+def normalize(myData, minData, maxData, minNorm=0, maxNorm=1):
     '''
-    Normalizes data from minData-maxData to the range min_norm-max-norm
+    Normalizes data from minData-maxData to the range minNorm-max-norm
     Builds a list of (datum, norm_datum) tuples
     '''
     my_list = list()
-    for datum in my_data:
-        norm_datum = (min_norm + (datum - minData) * (max_norm - min_norm)) / (maxData - minData)
+    for datum in myData:
+        norm_datum = (minNorm + (datum - minData) * (maxNorm - minNorm)) / (maxData - minData)
         my_list.append((datum, norm_datum))
     return(my_list)
-    
 
-def save_my_data(my_file, my_data, verbose_flag = False):
+
+def save_my_data(myFile, myData, verboseFlag = False):
     '''
     Save in a csv file
     '''
     print 'Saving data...'
-    my_output_file = my_file + '_out.csv'
+    my_output_file = myFile + '_out.csv'
     output_handle = open(my_output_file, 'w')
     output_handle.write('Data' + ',' + 'Transformed Data\n')
-    for datum, output_datum in my_data[:]:
+    for datum, output_datum in myData[:]:
         next_line = str(datum) + ',' + str(output_datum) + '\n'
-        if verbose_flag:
+        if verboseFlag:
             print next_line.rstrip()
         output_handle.write(next_line)
     output_handle.close()
@@ -123,13 +123,6 @@ def save_my_data(my_file, my_data, verbose_flag = False):
 
 
 
-    
+
 if __name__ == '__main__':
     main()
-
-    
-    
- 
-
-
-
