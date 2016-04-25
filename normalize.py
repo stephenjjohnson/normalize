a500d42d30bd14613e55da86a9268ed41bff7114
Stephen Johnson <stephenjjohnson@gmail.com>
2016-04-25 10:05:50 -0500

    Add GPL-3 license

--- normalize.py
+++ normalize.py
@@ -1,3 +1,19 @@
+#    normalize.py A Python program to normalize a dataset
+#    Copyright (C) 2016 Stephen J. Johnson
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 '''
 A Python program to normalize a dataset.
 Accepts a comma separated list of numbers then asks for the lowest and 
